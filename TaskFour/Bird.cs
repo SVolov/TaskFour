49e88d8e90822d8de22d1379f20bc3d0cd02f3d4
Sergei Volovik <sergei.volovik@icloud.com>
2023-06-07 16:26:03 +0300

    Add Program

---
+++ TaskFour/Bird.cs
@@ -0,0 +1,37 @@
+﻿namespace TaskFour
+{
+    public class Bird : IFlyable
+    {
+        //bird cannot rise above 3 km
+        private Coordinate currentPosition;
+
+        public Coordinate CurrentPosition { get { return currentPosition; } set { currentPosition = new Coordinate(value.X, value.Y < 3 ? value.Y : 3, value.Z); } }
+
+        public Bird(Coordinate currentPosition)
+        {
+            CurrentPosition = currentPosition;
+        }
+
+        public void FlyTo(Coordinate newPosition)
+        {
+            CurrentPosition = newPosition;
+        }
+
+        public double GetFlyTime(Coordinate newPosition)
+        {
+            if (newPosition.Y <= 3)
+            {
+                Random random = new Random();
+                int speed = random.Next(0, 21);
+                double distance = Math.Sqrt(Math.Pow(newPosition.X - CurrentPosition.X, 2) + Math.Pow(newPosition.Y - CurrentPosition.Y, 2) + Math.Pow(newPosition.Z - CurrentPosition.Z, 2));
+                double time = distance / speed;
+                return time;
+            }
+            else
+            {
+                Console.WriteLine("Bird can't fly that high");
+                return 0;
+            }
+        }
+    }
+}
--- TaskFour/Program.cs
+++ TaskFour/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+using TaskFour;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bird bird = new Bird(new Coordinate());
+        bird.FlyTo(new Coordinate(10, 21, 10));
+        bird.GetFlyTime(new Coordinate(100, 3, 300));
+
+        Plane plane = new Plane(new Coordinate());
+        plane.FlyTo(new Coordinate(10, 3, 10));
+        plane.GetFlyTime(new Coordinate(4000, 15, 4000));
+
+        Drone drone = new Drone(new Coordinate());
+        drone.FlyTo(new Coordinate(200, 0, 10));
+        drone.GetFlyTime(new Coordinate(3200, 10, 600));
+    }
+}
+
+public struct Coordinate
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    public Coordinate(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
